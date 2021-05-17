284225af831d1aa02a5a32729cd2ed10e0821f54
getafemean <getafemean@gmail.com>
2021-05-17 20:56:30 +0200

    padre hijo

--- src/app/marcador/marcador.component.ts
+++ src/app/marcador/marcador.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarcadorComponent implements OnInit {
 
-  local: any = {
+  equipoLocal: any = {
     nombre: 'lakers',
-    logo: ''
+    logo: '/assets/img/LAL.svg',
+    jugadores: [
+      {nombre: 'Isaiah Thomas', puntos: 0, imagen: '/assets/img/thomas.png'},
+      {nombre: 'Kyle Kuzma', puntos: 0, imagen: '/assets/img/kuzma.png'},
+      {nombre: 'Julius Randle', puntos: 0, imagen: '/assets/img/randle.png'},
+      {nombre: 'Brandom Ingram', puntos: 0, imagen: '/assets/img/ingram.png'},
+      {nombre: 'Brook López', puntos: 0, imagen: '/assets/img/lopez.png'},
+    ]
   }
+
+  equipoVisitante: any = {
+    nombre: 'celtics',
+    logo: '/assets/img/BOS.svg',
+    jugadores: [
+      {nombre: 'Kaadem Allen', puntos: 0, imagen: '/assets/img/allen.png'},
+      {nombre: 'Aron Bynes', puntos: 0, imagen: '/assets/img/bynes.png'},
+      {nombre: 'Jabari Bird', puntos: 0, imagen: '/assets/img/bird.png'},
+      {nombre: 'Jaylen Brown', puntos: 0, imagen: '/assets/img/brown.png'},
+      {nombre: 'Jonathan Gibson', puntos: 0, imagen: '/assets/img/gibson.png'},
+    ]
+  }
+
+  puntosLocales: number = 0;
+  puntosVisitantes: number = 0;
 
   constructor() { }
 
